475b0e34271323d6502bdf4f5fe94c6014ae6c5d
Lahirub99 <lahiru.19@cse.mrt.ac.lk>
2023-01-23 16:15:08 +0530

    55. Transaction Objects and the OutputMap | Tests

---
+++ wallet/transaction.test.js
@@ -0,0 +1,33 @@
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('Transaction', () => {
+    let transaction, senderWallet, recipient, amount;
+
+    beforeEach(() => {
+        senderWallet = new Wallet();
+        recipient = 'recipient-public-key';
+        amount = 50;
+
+        transaction = new Transaction({ senderWallet, recipient, amount });
+    });
+
+    it('has an `id`', () => {
+        expect(transaction).toHaveProperty(id);
+    });
+
+    describe('outputMap', () => {
+        it('has an `outputMap`', () => {
+            expect(transaction).toHaveProperty(outputMap);
+        });
+
+        it('output the amount to the reciepient', () => {
+            expect( transaction.outputMap[recipient] ).toEqual(amount);
+        });
+
+        it('output the reamining balance for the `senderWallet`', () => {
+            expect( transaction.outputMap[ senderWallet.publicKey] )
+                .toEqual( senderWallet.balance - amount );
+        });
+    });
+});
