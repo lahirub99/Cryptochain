e511ed5385a4d2ab168e01fbe45d6d9abd960a09
Lahirub99 <lahiru.19@cse.mrt.ac.lk>
2023-01-26 00:40:01 +0530

    71. Get Transaction Pool map

--- index.js
+++ index.js
@@ -42,9 +42,24 @@ app.post('/api/mine', (req, res) => {
 
 app.post('/api/transact', (req, res) => {
     const { amount, recipient } = req.body;
-    let transaction;
+
+    let transaction = transactionPool.existingTransaction({
+        inputAddress: wallet.publicKey
+    });    
+    // Making transaction dynamic
+    // Assigning a previously existed transaction to the transaction variable
+    
     try{
-        transaction = wallet.createTransaction({ recipient, amount });
+        if (transaction) {          // The transaction is defined
+            transaction.update({    // Transaction already exists --> Go through update process
+                senderWallet: wallet,
+                recipient,
+                amount
+            }); 
+        } else {    // The transaction is undefined
+            transaction = wallet.createTransaction({ recipient, amount });
+        }
+        
     } catch (error) {
         return res.status(400).json({
             type: 'error',
