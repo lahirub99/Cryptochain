7dbff9d4c27757d9db384993385f850dcb3d377b
Lahirub99 <lahiru.19@cse.mrt.ac.lk>
2023-01-26 02:26:53 +0530

    72. Broadcast Transaction

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -69,11 +69,12 @@ app.post('/api/transact', (req, res) => {
 
     transactionPool.setTransaction( transaction );
 
-    console.log('transactionPool', transactionPool);
+    pubsub.broadcastTransaction(transaction);
+
+    // console.log('transactionPool', transactionPool);
 
     res.json({ type: 'success', transaction });
 });
-
 const syncChains = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
@@ -84,6 +85,10 @@ const syncChains = () => {
         }
     });
 };
+
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
+});
 
 let PEER_PORT;     // Not defined
 
