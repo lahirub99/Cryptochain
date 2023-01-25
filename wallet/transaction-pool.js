e5086724e8e967cb845cc022a3ffe707e14900cd
Lahirub99 <lahiru.19@cse.mrt.ac.lk>
2023-01-26 00:06:20 +0530

    70. Transaction Updates in the API

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -6,6 +6,19 @@ class TransactionPool {
     setTransaction(transaction) {
         this.transactionMap [transaction.id] = transaction;
     }
+
+    existingTransaction({ inputAddress }) {
+        // Goal is to return the transaction that exist 
+        // for this input address if there's one already
+        // stored in the transaction map.
+        const transactions = Object.values( this. transactionMap );
+
+        return transactions.find(
+                transaction => transaction.input.address === inputAddress
+            );
+            // Go through the transactions and returns the 1st transaction 
+            // where matching occurs in the input address.
+    }
 }
 
 module.exports = TransactionPool;
