4057b47ed32fa08b3c47001c647b8ed417c48d1b
Lahirub99 <lahiru.19@cse.mrt.ac.lk>
2023-01-25 13:46:45 +0530

    68.  API Transactions and Main Transaction Pool

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -35,6 +39,16 @@ app.post('/api/mine', (req, res) => {
     //Gives confirmation to the requester -> Can see new data block is added to the chain
     res.redirect('/api/blocks');
 })
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+    const transaction = wallet.createTransaction({ recipient, amount });
+    transactionPool.setTransaction( transaction );
+
+    console.log('transactionPool', transactionPool);
+
+    res.json({ transaction });
+});
 
 const syncChains = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
